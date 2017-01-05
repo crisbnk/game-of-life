1e922f13df834a09219e14213199ea7d18a4d850
Cristiano Bianchi <cristiano.bianchi@gmail.com>
2017-01-05 23:59:01 +0100

    Added cell position

--- src/js/client.js
+++ src/js/client.js
@@ -2,13 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Square extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      value: 0,
-    };
-  }
-
   render() {
     return (
       <button className="square" onClick={() => this.props.onClick()}>
@@ -23,7 +16,11 @@ class LifeTable extends React.Component {
     super();
     // Need to create Array of Array automatically (and give user the possibility to set it)
     this.state = {
-      squares: [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+      squares: [['00','01','02','03','04'],
+      ['10','11','12','13','14'],
+      ['20','21','22','23','24'],
+      ['30','31','32','33','34'],
+      ['40','41','42','43','44']]
     };
   }
 
@@ -33,6 +30,23 @@ class LifeTable extends React.Component {
     this.setState({squares: squares});
   }
 
+  giveLife() {
+    let squares = this.state.squares;
+    for(let i = 0; i < squares.length; i++) {
+      console.log(squares[i]);
+      for(let j = 0; j < squares[i].length; j++) {
+        const n = i - 1 > -1 ? i - 1 : i + (squares.length - 1);
+        const s = i + 1 < squares.length ? i + 1 : i - (squares.length - 1);
+        const o = j - 1 > -1 ? j - 1 : j + (squares.length - 1);
+        const e = j + 1 < squares.length ? j + 1 : j - (squares.length - 1);
+        console.log(`${squares[i][j]}, sopra: ${squares[n][j]}, sotto: ${squares[s][j]}, destra: ${squares[i][e]}, sinistra: ${squares[i][o]}, NE: ${squares[n][e]}, NO: ${squares[n][o]}`);
+        const neighborhood = [squares[n][j], squares[s][j], squares[i][e], squares[i][o], squares[n][e], squares[n][o], squares[s][e], squares[s][o]];
+        const tot = neighborhood.reduce((a, b) => a + b);
+        console.log(`squares ${i}${j} neighborhood is: ${tot}`);
+      }
+    }
+  }
+
   renderSquare(i, j) {
     return <Square value={this.state.squares[i][j]} onClick={() => this.handleClick(i, j)} />;
   }
@@ -40,41 +54,46 @@ class LifeTable extends React.Component {
   render() {
     return (
       <div>
-        {/* Need to create the board automatically from user input */}
-        <div className="board-row">
-          {this.renderSquare(0,0)}
-          {this.renderSquare(0,1)}
-          {this.renderSquare(0,2)}
-          {this.renderSquare(0,3)}
-          {this.renderSquare(0,4)}
+        <div>
+          {/* Need to create the board automatically from user input */}
+          <div className="board-row">
+            {this.renderSquare(0,0)}
+            {this.renderSquare(0,1)}
+            {this.renderSquare(0,2)}
+            {this.renderSquare(0,3)}
+            {this.renderSquare(0,4)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(1,0)}
+            {this.renderSquare(1,1)}
+            {this.renderSquare(1,2)}
+            {this.renderSquare(1,3)}
+            {this.renderSquare(1,4)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(2,0)}
+            {this.renderSquare(2,1)}
+            {this.renderSquare(2,2)}
+            {this.renderSquare(2,3)}
+            {this.renderSquare(2,4)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3,0)}
+            {this.renderSquare(3,1)}
+            {this.renderSquare(3,2)}
+            {this.renderSquare(3,3)}
+            {this.renderSquare(3,4)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(4,0)}
+            {this.renderSquare(4,1)}
+            {this.renderSquare(4,2)}
+            {this.renderSquare(4,3)}
+            {this.renderSquare(4,4)}
+          </div>
         </div>
-        <div className="board-row">
-          {this.renderSquare(1,0)}
-          {this.renderSquare(1,1)}
-          {this.renderSquare(1,2)}
-          {this.renderSquare(1,3)}
-          {this.renderSquare(1,4)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(2,0)}
-          {this.renderSquare(2,1)}
-          {this.renderSquare(2,2)}
-          {this.renderSquare(2,3)}
-          {this.renderSquare(2,4)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3,0)}
-          {this.renderSquare(3,1)}
-          {this.renderSquare(3,2)}
-          {this.renderSquare(3,3)}
-          {this.renderSquare(3,4)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(4,0)}
-          {this.renderSquare(4,1)}
-          {this.renderSquare(4,2)}
-          {this.renderSquare(4,3)}
-          {this.renderSquare(4,4)}
+        <div className="button" onClick={() => this.giveLife()}>
+          <button>Procedi</button>
         </div>
       </div>
     );
