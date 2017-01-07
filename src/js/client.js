530cc39099ce75f22615a95c705564ba985a1ddb
Cristiano Bianchi <cristiano.bianchi@gmail.com>
2017-01-07 11:10:43 +0100

    Added ESLint

--- src/js/client.js
+++ src/js/client.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square" onClick={() => this.props.onClick()}>
+      <button className='square' onClick={() => this.props.onClick()}>
         {this.props.value}
       </button>
     );
@@ -14,7 +14,8 @@ class Square extends React.Component {
 class LifeTable extends React.Component {
   constructor() {
     super();
-    // Need to create Array of Array automatically (and give user the possibility to set it)
+    // Need to create Array of Array automatically
+    // (and give user the possibility to set it)
     const rows = 10;
     const cols = 10;
     const squares = Array(rows).fill().map(() => Array(cols).fill(0));
@@ -39,51 +40,62 @@ class LifeTable extends React.Component {
     let newValues = this.createMatrix(this.state.rows, this.state.cols);
     console.log(newValues);
     let squares = this.state.squares;
-    for(let i = 0; i < squares.length; i++) {
+    for (let i = 0; i < squares.length; i++) {
       console.log(squares[i]);
-      for(let j = 0; j < squares[i].length; j++) {
+      for (let j = 0; j < squares[i].length; j++) {
         const value = squares[i][j];
         const n = i - 1 > -1 ? i - 1 : i + (squares.length - 1);
         const s = i + 1 < squares.length ? i + 1 : i - (squares.length - 1);
         const o = j - 1 > -1 ? j - 1 : j + (squares.length - 1);
         const e = j + 1 < squares.length ? j + 1 : j - (squares.length - 1);
-        console.log(`${value}, sopra: ${squares[n][j]}, sotto: ${squares[s][j]}, destra: ${squares[i][e]}, sinistra: ${squares[i][o]}, NE: ${squares[n][e]}, NO: ${squares[n][o]}`);
-        const neighborhood = [squares[n][j], squares[s][j], squares[i][e], squares[i][o], squares[n][e], squares[n][o], squares[s][e], squares[s][o]];
+        console.log(`${value}, sopra: ${squares[n][j]}, sotto: ${squares[s][j]},
+           destra: ${squares[i][e]}, sinistra: ${squares[i][o]},
+           NE: ${squares[n][e]}, NO: ${squares[n][o]}`);
+        const neighborhood = [
+          squares[n][j], squares[s][j], squares[i][e],
+          squares[i][o], squares[n][e], squares[n][o],
+          squares[s][e], squares[s][o]
+        ];
         const tot = neighborhood.reduce((a, b) => a + b);
         console.log(`squares ${i}${j} neighborhood is: ${tot}`);
-        if(value === 0 && tot === 3) {
+        if (value === 0 && tot === 3) {
           newValues[i][j] = 1;
-        } else if(value === 1 && (tot >= 2 && tot <= 3)) {
+        } else if (value === 1 && (tot >= 2 && tot <= 3)) {
           newValues[i][j] = 1;
         } else {
           newValues[i][j] = 0;
         }
       }
-      console.log(`New values are: `, newValues);
+      console.log('New values are: ', newValues);
       this.setState({squares: newValues});
     }
   }
 
   renderSquare(i, j) {
-    return <Square value={this.state.squares[i][j]} onClick={() => this.handleClick(i, j)} />;
+    return (
+      <Square
+        onClick={() => this.handleClick(i, j)}
+        value={this.state.squares[i][j]}
+      />
+    );
   }
 
   renderBoard() {
     const squares = this.state.squares;
     const rows = squares.map((row, i) => {
       const cellsInRow = row.map((cell, j) => {
-        return <span key={i + "" + j}>{this.renderSquare(i,j)}</span>;
+        return <span key={i + '' + j}>{this.renderSquare(i, j)}</span>;
       });
-      return <div key={i} className="board-row">{cellsInRow}</div>;
+      return <div className='board-row' key={i}>{cellsInRow}</div>;
     });
-    return <div>{rows}</div>
+    return <div>{rows}</div>;
   }
 
   render() {
     return (
       <div>
         {this.renderBoard()}
-        <div className="button" onClick={() => this.giveLife()}>
+        <div className='button' onClick={() => this.giveLife()}>
           <button>Procedi</button>
         </div>
       </div>
@@ -94,12 +106,12 @@ class LifeTable extends React.Component {
 class Main extends React.Component {
   render() {
     return (
-      <div className="main">
+      <div className='main'>
         <h1>Game of Life</h1>
-        <div className="lifeTable">
+        <div className='lifeTable'>
           <LifeTable />
         </div>
-        <div className="settings">
+        <div className='settings'>
           <p>Settings</p>
         </div>
       </div>
